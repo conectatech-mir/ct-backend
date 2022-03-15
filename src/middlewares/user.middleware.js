45a9844d7c3480745ce10e087ada743e9f5027ba
Junior Almeyda <jr.almeyda7@gmail.com>
2022-03-14 19:47:11 -0500

    agrega el controlador del usuario

---
+++ src/middlewares/user.middleware.js
@@ -0,0 +1,22 @@
+const { validationResult } = require('express-validator')
+const User = require('../models/user.model')
+
+const validateField = (req, res, next) => {
+  const errors = validationResult(req);
+  if( !errors.isEmpty() ){
+    return res.status(400).json(errors);
+  }
+  next()
+}
+
+const existsUserById = async(id) => {
+  const existsUser = await User.findById(id)
+  if( !existsUser ) {
+    throw new Error(`El id ${id} no existe en la BD`)
+  }
+}
+
+module.exports = {
+  validateField,
+  existsUserById
+}
