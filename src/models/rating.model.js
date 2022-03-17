f8140b1fa9af1b92869ac18a2db8a47ee806193d
Jhon Achata Limachi <jhonal183@gmail.com>
2022-03-17 12:11:28 -0500

    add validations for create rating endpoint

--- src/models/rating.model.js
+++ src/models/rating.model.js
@@ -4,14 +4,17 @@ const ratingSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
     ref: 'User',
+    required: true,
   },
   professional: {
     type: Schema.Types.ObjectId,
     ref: 'User',
+    required: true,
   },
   post: {
     type: Schema.Types.ObjectId,
     ref: 'Post',
+    required: true,
   },
   body: {
     type: String,
--- src/routes/ratings.routes.js
+++ src/routes/ratings.routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { body } = require('express-validator')
 const { list, store } = require('../controllers/rating.controller')
 const { authenticateToken } = require('../middlewares/auth.middleware')
 const { validateField, isSameUser } = require('../middlewares/user.middleware')
@@ -6,6 +7,15 @@ const { validateField, isSameUser } = require('../middlewares/user.middleware')
 const router = express.Router()
 
 router.get('/', authenticateToken, list) // TODO: Remove this route
-router.post('/', [authenticateToken, isSameUser, validateField], store)
+router.post('/', [
+  authenticateToken,
+  body('user').isMongoId().withMessage('User is not valid'),
+  isSameUser,
+  body('professional').isMongoId().withMessage('Professional is not valid'),
+  body('post').isMongoId().withMessage('Post is not valid'),
+  body('body').isLength({ min: 5 }).withMessage('The comment must have at least 5 characters'),
+  body('value').isInt({ min: 1, max: 5 }).withMessage('The value must be between 1 and 5'),
+  validateField
+], store)
 
 module.exports = router
