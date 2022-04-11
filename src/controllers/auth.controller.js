4628e8e0c66554e712a830df060896f57dfcba1d
Junior Almeyda <jr.almeyda7@gmail.com>
2022-04-11 02:11:05 -0500

    añadiendo skills en user.model y añadiendo skills en auth.controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,47 +1,57 @@
-const { validationResult } = require('express-validator')
-const jwt = require('jsonwebtoken')
-const User = require("../models/user.model")
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
 
-const {sendEmail} = require('../middlewares/sendGridMail.middleware')
+const { sendEmail } = require("../middlewares/sendGridMail.middleware");
 
 const login = async (req, res) => {
-  const { email } = req.body
+  const { email } = req.body;
 
   try {
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email });
 
-    const token = jwt.sign({
-      email: user.email,
-      id: user._id
-    }, process.env.SECRET_KEY || 'secret', { expiresIn: '1h' })
+    const token = jwt.sign(
+      {
+        email: user.email,
+        id: user._id,
+      },
+      process.env.SECRET_KEY || "secret",
+      { expiresIn: "1h" }
+    );
 
-    res.status(200)
-      .json({
-        ok: true,
-        message: 'User logged in successfully',
-        data: {
-          id: user._id,
-          email: user.email,
-          token,
-        }
-      })
+    res.status(200).json({
+      ok: true,
+      message: "User logged in successfully",
+      data: {
+        id: user._id,
+        email: user.email,
+        token,
+      },
+    });
   } catch (err) {
-    res.status(400)
-      .json({
-        ok: false,
-        message: err,
-      })
+    res.status(400).json({
+      ok: false,
+      message: err,
+    });
   }
-}
+};
 
 const register = async (req, res) => {
-  const { firstName, lastName, email, password, rol = 'USER', phone = null, about = null } = req.body
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    rol = "USER",
+    phone = null,
+    about = null,
+  } = req.body;
 
   try {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const newUser = await User.create({
@@ -52,32 +62,39 @@ const register = async (req, res) => {
       rol,
       phone,
       about,
-    })
+    });
 
     await sendEmail(newUser);
 
-    res.status(201)
-      .json({
-        ok: true,
-        message: 'User has been created successfully',
-      })
+    res.status(201).json({
+      ok: true,
+      message: "User has been created successfully",
+    });
   } catch (err) {
-    res.status(500)
-      .json({
-        ok: false,
-        message: err,
-      })
+    res.status(500).json({
+      ok: false,
+      message: err,
+    });
   }
-}
+};
 
 const professionalRegister = async (req, res) => {
-  const { firstName, lastName, email, password, rol = 'PROFESSIONAL', phone = null, about = null } = req.body
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    rol = "PROFESSIONAL",
+    phone = null,
+    about = null,
+    skills = [],
+  } = req.body;
 
   try {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const newUser = await User.create({
@@ -88,34 +105,32 @@ const professionalRegister = async (req, res) => {
       rol,
       phone,
       about,
-    })
+      skills,
+    });
 
-    res.status(201)
-      .json({
-        ok: true,
-        message: 'response message register',
-        data: newUser
-      })
+    res.status(201).json({
+      ok: true,
+      message: "response message register",
+      data: newUser,
+    });
   } catch (err) {
-    res.status(500)
-      .json({
-        ok: false,
-        message: err,
-      })
+    res.status(500).json({
+      ok: false,
+      message: err,
+    });
   }
-}
+};
 
 const logout = async (req, res) => {
-  res.status(204)
-    .json({
-      ok: true,
-      message: 'response message logout'
-    })
-}
+  res.status(204).json({
+    ok: true,
+    message: "response message logout",
+  });
+};
 
 module.exports = {
   login,
   register,
   professionalRegister,
-  logout
-}
+  logout,
+};
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,57 +1,64 @@
-const bcrypt = require('bcrypt')
-const { Schema, model } = require('mongoose')
+const bcrypt = require("bcrypt");
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  rol: {
-    type: String,
-    enum: ['USER', 'PROFESSIONAL'],
-    default: 'USER',
-  },
-  phone: {
-    type: String,
-  },
-  about: {
-    type: String,
-  },
-  ratings: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: 'Rating',
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
       required: true,
-    }
-  ]
-}, {
-  timestamps: true,
-})
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    rol: {
+      type: String,
+      enum: ["USER", "PROFESSIONAL"],
+      default: "USER",
+    },
+    phone: {
+      type: String,
+    },
+    about: {
+      type: String,
+    },
+    ratings: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Rating",
+        required: true,
+      },
+    ],
+    skills: {
+      type: Array,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.statics.hashPassword = async (password) => {
-  const salt = await bcrypt.genSalt(10)
+  const salt = await bcrypt.genSalt(10);
 
-  return await bcrypt.hash(password, salt)
-}
+  return await bcrypt.hash(password, salt);
+};
 
 userSchema.methods.comparePassword = async (password, hash) => {
-  return await bcrypt.compare(password, hash)
-}
+  return await bcrypt.compare(password, hash);
+};
 
-userSchema.virtual('fullName')
-  .get(() => this.firstName + ' ' + this.lastName)
-  .set((v) => this.firstName + ' ' + this.lastName)
+userSchema
+  .virtual("fullName")
+  .get(() => this.firstName + " " + this.lastName)
+  .set((v) => this.firstName + " " + this.lastName);
 
-module.exports = model('User', userSchema)
+module.exports = model("User", userSchema);
