7d4aa32ad7a07d0b5a1437d124f2634b8e268207
Junior Almeyda <jr.almeyda7@gmail.com>
2022-04-20 22:26:55 -0500

    modificando auth controller

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,48 +1,48 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user.model')
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
 
 const emailAlreadyInUse = (email) => {
-  return User.findOne({ email }).then(user => {
+  return User.findOne({ email }).then((user) => {
     if (user) {
-      return Promise.reject('E-mail already in use');
+      return Promise.reject("E-mail already in use");
     }
   });
-}
+};
 
 const userWithEmailExist = (email) => {
-  return User.findOne({ email }).then(user => {
+  return User.findOne({ email }).then((user) => {
     if (!user) {
-      return Promise.reject('E-mail not found');
+      return Promise.reject("E-mail not found");
     }
-  })
-}
+  });
+};
 
 const userPasswordMatch = (password, { req }) => {
   return User.findOne({ email: req.body.email }).then(async (user) => {
-    if (! await user.comparePassword(password, user.password)) {
-      return Promise.reject('Wrong username or password')
+    if (!(await user.comparePassword(password, user.password))) {
+      return Promise.reject("Wrong username or password");
     }
-  })
-}
+  });
+};
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers['authorization']
+  const authHeader = req.headers["authorization"];
   // console.log(authHeader);
-  const token = authHeader && authHeader.split(' ')[1]
-  console.log(token);
-  if (token === null) return res.status(401).json({ error: 'Unauthorized' })
+  const token = authHeader && authHeader.split(" ")[1];
+  // console.log(token);
+  if (token === null) return res.status(401).json({ error: "Unauthorized" });
 
-  jwt.verify(token, process.env.SECRET_KEY || 'secret', (err, user) => {
+  jwt.verify(token, process.env.SECRET_KEY || "secret", (err, user) => {
     console.log(err);
-    if (err) return res.status(403).json({ error: 'Forbidden' })
+    if (err) return res.status(403).json({ error: "Forbidden" });
 
-    next()
-  })
-}
+    next();
+  });
+};
 
 module.exports = {
   emailAlreadyInUse,
   userWithEmailExist,
   userPasswordMatch,
   authenticateToken,
-}
+};
